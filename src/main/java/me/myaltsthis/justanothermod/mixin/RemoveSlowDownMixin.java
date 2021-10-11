623eadb201fd1027a49cb4cc34b7b505ceb31083
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2021-10-11 14:43:48 -0500

    first release?

--- src/main/java/me/myaltsthis/justanothermod/mixin/RemoveSlowDownMixin.java
+++ src/main/java/me/myaltsthis/justanothermod/mixin/RemoveSlowDownMixin.java
@@ -8,6 +8,8 @@ import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerEntity.class)
@@ -17,9 +19,24 @@ public abstract class RemoveSlowDownMixin extends AbstractClientPlayerEntity {
         super(clientWorld, gameProfile);
     }
 
-    @Inject(at = @At("HEAD"), method = "shouldSlowDown", cancellable = true)
+    @Inject(method = "shouldSlowDown", at = @At("HEAD"), cancellable = true)
     private void changeReturn(CallbackInfoReturnable<Boolean> cir) {
         if (JustAnotherModClient.isButtonEnabled())
             cir.setReturnValue(false);
     }
+
+    @Redirect(
+            method = "tickMovement",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isUsingItem()Z"),
+            slice = @Slice(
+                    from = @At(value = "FIELD", target = "Lnet/minecraft/client/input/Input;jumping:Z"),
+                    to = @At(value = "FIELD", target = "Lnet/minecraft/client/network/ClientPlayerEntity;ticksToNextAutojump:I")
+            )
+    )
+    private boolean notUsingItem(ClientPlayerEntity instance) {
+        if (JustAnotherModClient.isButtonEnabled())
+            return false;
+        return instance.isUsingItem();
+    }
 }
+
