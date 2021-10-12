017c84c1d679acd4f2f30e88d9dc2bbd9cd8af0d
myaltaccountsthis <71832770+myaltaccountsthis@users.noreply.github.com>
2021-10-11 21:02:29 -0500

    fixed FluidMixin

--- src/main/java/me/myaltsthis/justanothermod/mixin/FluidMixin.java
+++ src/main/java/me/myaltsthis/justanothermod/mixin/FluidMixin.java
@@ -3,26 +3,37 @@ package me.myaltsthis.justanothermod.mixin;
 import me.myaltsthis.justanothermod.client.JustAnotherModClient;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class FluidMixin {
+    @Shadow public float flyingSpeed;
+
     @ModifyVariable(method = "travel", at = @At("STORE"), ordinal = 2)
-    private float modifyH(float h) {
-        if (JustAnotherModClient.isButtonEnabled()) {
+    private float modifyWater(float h) {
+        if (JustAnotherModClient.isButtonEnabled() && ((LivingEntity)(Object) this).getClass().getSimpleName().equals("ClientPlayerEntity"))
             return 3F;
-        }
         return h;
     }
 
     @ModifyConstant(method = "travel", constant = @Constant(floatValue = 0.02F), slice = @Slice(
             from = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;isInLava()Z")
     ))
-    private float modifyConstant(float f) {
-        if (JustAnotherModClient.isButtonEnabled()) {
-            //System.out.println("f: " + f);
+    private float modifyLava(float f) {
+        if (JustAnotherModClient.isButtonEnabled() && ((LivingEntity)(Object) this).getClass().getSimpleName().equals("ClientPlayerEntity"))
             return .1F;
-        }
         return f;
     }
+    /*
+    @Inject(method = "getMovementSpeed(F)F", at = @At("RETURN"), cancellable = true)
+    private void modifyMovementSpeed(float slipperiness, CallbackInfoReturnable<Float> cir) {
+        LivingEntity e = (LivingEntity) (Object) this;
+        if (JustAnotherModClient.isButtonEnabled() && e.getClass().getSimpleName().equals("ClientPlayerEntity")) {
+
+            cir.setReturnValue(e.isOnGround() ? e.getMovementSpeed() : flyingSpeed);
+        }
+    }
+     */
 }
